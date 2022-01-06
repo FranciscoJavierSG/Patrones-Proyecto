0cc4e3d4b64e0a31864d855d861fd7255258f084
Francisco <fsalazarg@ing.ucsc.cl>
2022-01-06 02:13:53 -0300

    Estructura del State terminado
    Falta completar las funciones, pero la estructura del State está lista

---
+++ proyecto_patrones/models/State/TipoVenta.class.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace StateProyecto;
+
+class TipoVenta {
+
+    public static function setTipoVenta($tipoVenta) {
+        if ($tipoVenta <= 1) {
+            return 'Para Llevar';
+        }
+        if ($tipoVenta >= 2) {
+            return 'En local';
+        }
+    }
+}
--- proyecto_patrones/models/State/Venta.class.php
+++ proyecto_patrones/models/State/Venta.class.php
@@ -1,51 +1,51 @@
 <?php
-namespace State;
+
+namespace StateProyecto;
+
 /*
 require_once 'PedidoActual.class.php';
-require_once 'Producto.class.php';
+require_once 'Restaurant.class.php';
 require_once 'ListaProducto.class.php';
 */
-// esta es Venta  AJUSTAR a ESTE PROYECTO
-class Venta
-{
-    /**
-     * @var ListaProducto
-     */
-    protected $productos;
-    /**
-     * 
-     * @var EstadoPedido
-     */
-    protected $estadoPedido;
+require_once 'EstadoVenta.class.php';
+require_once 'TipoVenta.class.php';
+require_once 'Restaurant.class.php';
 
-    public function __construct()
-    {
-        $this->productos = new ListaProducto();
-        $this->estadoPedido = new PedidoActual($this);
+class Venta {
+
+    protected $id_venta; //Falta la asignación del id
+    protected $estadoVenta;
+    protected $id_pedido; //id del pedido que se entrega en la tabla Pedido a la tabla Venta
+
+    public function __construct() {
+        //$this->id_venta = ????????
+        $this->estadoVenta = new TipoVenta();
+        $this->id_pedido = new Pedido();
     }
 
-    /**
-     * 
-     * @param Producto $producto
-     */
-    public function estadoSiguiente(){
-    }
-    public function getEstado(){
-    }
-    public function crearVenta(){
-    }
-    public function verVenta(){
-    }
-    public function editarVenta(){
-    }
-    public function getHistorialVenta(){
-    }
-    public function eliminarVenta(){
+    public function estadoSiguiente() {
     }
 
+    public function getEstado() {
+    }
 
+    public function crearVenta() {
+    }
 
-/*
+    public function verVenta() {
+    }
+
+    public function editarVenta() {
+    }
+
+    public function getHistorialVenta() {
+    }
+
+    public function eliminarVenta() {
+        $this->id_pedido->eliminarVenta();
+    }
+
+    /*
     public function agregaProducto(Producto $producto)
     {
         $this->estadoPedido->agregaProducto($producto);
@@ -55,7 +55,7 @@ class Venta
      *
      * @param Producto $producto
      */
-/*
+    /*
     public function retiraProducto(Producto $producto)
     {
         $this->estadoPedido->retiraProducto($producto);
@@ -74,7 +74,7 @@ class Venta
     /**
      * @return ListaProducto
      */
-/*
+    /*
     public function getProductos()
     {
         return $this->productos;
@@ -92,5 +92,3 @@ class Venta
     }
 */
 }
-
-?>
