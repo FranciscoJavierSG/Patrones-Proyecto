b8ddfd6e80745cf11193143e1474cf6907662c6b
Abysmal6 <41496239+Abysmal6@users.noreply.github.com>
2022-01-10 17:00:17 -0300

    Update Ingredientes.class.php

--- proyecto_patrones/Proyecto/patrones/abstractfactory/Ingredientes.class.php
+++ proyecto_patrones/Proyecto/patrones/abstractfactory/Ingredientes.class.php
@@ -11,21 +11,51 @@ class Ingredientes {
      * @return Ingrediente
      */
 
+     public $stock;
+     public $nombre;
+     public $tipoIngrediente;
+
     //Habria que definir bien esto,  
     public function agregarIngrediente($stock,$nombre,$tipoIngrediente){
-
+        $this->stock = $stock;
+        $this->nombre = $nombre;
+        $this->tipoIngrediente = $tipoIngrediente;
     }
     public function eliminarIngrediente($stock,$nombre,$tipoIngrediente){
+        //Esta cosa seria algo asi como
+        //Ingrediente tomate= null;?
 
     }
     public function verIngrediente($stock,$nombre,$tipoIngrediente){
 
+        $this->stock = $stock;
+        $this->nombre = $nombre;
+        $this->tipoIngrediente = $tipoIngrediente;
+
+        return array(
+            'Stock '=> $this->stock,
+            'Nombre' => $this->nombre,
+            'Tipo de ingrediente' => $this->tipoIngrediente
+            );
     }
     public function modificarIngrediente($stock,$nombre,$tipoIngrediente){
 
+        //Esta deberia modificar el ingrediente y a la vez servir para modificar el stock aumentandolo
+        
+
+
     }
     public function modificarStock($stock,$nombre,$tipoIngrediente){
-        
+        //Esto elimina el stock de un ingrediente
+        $this->stock = $stock;
+        $this->nombre = $nombre;
+        $this->tipoIngrediente = $tipoIngrediente;
+        if($this->stock <=0){
+            return 'no hay stock';
+        }else{
+            $this->stock = $stock-1;
+        }
+
     }
 
 }
