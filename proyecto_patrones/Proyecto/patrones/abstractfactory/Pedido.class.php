794c18e6a9adcab9c619239b1cc4f5977cbbd168
Abysmal6 <41496239+Abysmal6@users.noreply.github.com>
2022-01-10 21:40:55 -0300

    JEJEJEJEJEJERJEJEJEJEJEJEJEJEJERJEJEJEJEJEJEJEEJEJEJEJEJEJEJEJEEJEJEJEJEJEJE

---
+++ proyecto_patrones/Proyecto/patrones/abstractfactory/Pedido.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace Proyecto;
+
+require_once 'PlatoEntrada.class.php';
+require_once 'PlatoFondo.class.php';
+require_once 'Venta.class.php';
+
+class EntradaCarnica 
+{
+
+    public int $id_plato;
+    public String $nombre;
+    public int $precio;
+
+    public function __construct($id_plato, $nombre, $precio)
+    {
+        $this->id_plato = $id_plato;
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+      
+    }
+    public function mostrarPedido($id_plato,$nombre,$precio){
+        return array(
+            'id plato' => $this->$id_plato,
+            'Nombre' => $this->$id_plato,
+            'Precio' => $this->$precio
+        );
+    }
+
+}
+
+?>
