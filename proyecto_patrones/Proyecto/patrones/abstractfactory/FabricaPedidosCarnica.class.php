c6a7b0f1cf2c082a8628452e9492c9bcff8a1afe
Abysmal6 <41496239+Abysmal6@users.noreply.github.com>
2022-01-10 23:03:31 -0300

    asdasdasd

--- proyecto_patrones/Proyecto/patrones/abstractfactory/FabricaPedidosCarnica.class.php
+++ proyecto_patrones/Proyecto/patrones/abstractfactory/FabricaPedidosCarnica.class.php
@@ -4,35 +4,37 @@ namespace Proyecto;
 require_once 'FabricaPedidos.class.php';
 require_once 'EntradaCarnica.class.php';
 require_once 'FondoCarnico.class.php';
-require_once 'Ingredientes.class.php';
+require_once 'Inventario.class.php';
 
 class FabricaPedidosCarnica implements FabricaPedidos
 {
 
     /**
      *
-     * @param array $ingredientes        
+     * @param array $ingredientes         
      * @param int $id_plato     
-     * @param string $nombre         
+     * @param string $nombre     
+     * @param int $precio           
      * @return EntradaCarnica
      */
 
-
-    public function crearPlatoEntrada($ingredientes, $id_plato, $nombre)
+    public function crearPlatoEntrada($ingredientes, $id_plato, $nombre, $precio)
     {
-        return new EntradaCarnica($ingredientes, $id_plato, $nombre);
+        return new EntradaCarnica($ingredientes, $id_plato, $nombre, $precio);
     }
 
     /**
      *
-     * @param array $ingredientes          
+     * @param array $ingredientes        
      * @param int $id_plato     
-     * @param string $nombre         
-     * @return FondoCarnico
+     * @param string $nombre    
+     * @param int $precio          
+     * @return FondoCarnica
      */
-    public function crearPlatoFondo($ingredientes, $id_plato, $nombre)
+    public function crearPlatoFondo($ingredientes, $id_plato, $nombre, $precio)
     {
-        return new FondoCarnico($ingredientes, $id_plato, $nombre);
+        
+        return new FondoCarnica($ingredientes, $id_plato, $nombre ,$precio);
     }
 
 }
---
+++ proyecto_patrones/Proyecto/patrones/abstractfactory/Inventario.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace Proyecto;
+
+require_once 'Ingrediente.class.php';
+
+
+class Inventario {
+
+    /**
+     *
+     * @param bool $disponible                         
+     *     
+     * return $disponible
+     */
+   
+
+     //Mi duda es como entregarle los ingredientes a esta clase (podria usar un constructor? 
+     // podria sacarlos implementando los metodos de ingrediente tambien!)
+     public function eliminarIngrediente();
+     public function modificiarIngrediente();
+     public function modificarStock();
+
+     //Esta funcion deberia mostrar los ingredientes
+     public function verIngrediente();
+
+     //Esta funcion retorna el booleano
+     public function validarStockIngrediente();
+
+
+}
+
+?>
