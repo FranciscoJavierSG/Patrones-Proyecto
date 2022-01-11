9ff81553a98ecffb67dc3d521f8f3c36d49aa3b7
Francisco <fsalazarg@ing.ucsc.cl>
2022-01-11 13:26:29 -0300

    Update Venta.class.php

--- proyecto_patrones/proyecto_este_si/models/Venta.class.php
+++ proyecto_patrones/proyecto_este_si/models/Venta.class.php
@@ -3,15 +3,14 @@
 namespace Proyecto;
 
 require_once 'EstadoVenta.class.php';
-require_once 'TipoVenta.class.php';
+//require_once 'TipoVenta.class.php';
 require_once 'Restaurant.class.php';
-//require_once 'Cliente.class.php';
 require_once 'Pedido.class.php';
 
 class Venta {
 
     protected $venta;
-    //protected $pedido;
+    protected $pedido; //esto estaba comentado
 
     public function __construct(Pedido $Pedido) {
         $this->venta = new IniciarVenta($this);
@@ -22,16 +21,15 @@ class Venta {
         $this->venta = $this->venta->estadoSiguiente();
     }
 
-    public function getEstado($venta) {
+    public function getEstado() {
         return $this->venta; // no sé si ta bueno
     }
 
     public function verVenta() {
         //$this->venta->verVenta();
         return array(
-            'id_plato' =>  $this->id_plato,
-            'nombre' => $this->nombre,
-            'precio' => $this->precio,
+            'venta' =>  $this->venta,
+            'pedido' => $this->pedido
         );
     }
 
