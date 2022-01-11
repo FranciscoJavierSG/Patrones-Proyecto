c5289c5cf928527ba991a9d6726d7fc75df6432e
Francisco <fsalazarg@ing.ucsc.cl>
2022-01-11 14:13:42 -0300

    Update FabricaPedidos.class.php

--- proyecto_patrones/proyecto_este_si/models/FabricaPedidos.class.php
+++ proyecto_patrones/proyecto_este_si/models/FabricaPedidos.class.php
@@ -4,21 +4,7 @@ namespace Proyecto;
 
 interface FabricaPedidos {
 
-    /**
-     * @param array $ingredientes   
-     * @param int $id_plato     
-     * @param string $nombre  
-     * @param int $precio                          
-     * @return PlatoEntrada
-     */
     public function crearPlatoEntrada($ingredientes, $id_plato, $nombre, $precio);
 
-    /**
-     * @param array $ingredientes  
-     * @param int $id_plato     
-     * @param string $nombre
-     * @param int $precio                
-     * @return PlatoFondo
-     */
     public function crearPlatoFondo($ingredientes, $id_plato, $nombre, $precio);
 }
